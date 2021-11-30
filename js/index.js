f8bb24dde23ffe74396a218668171d9758b031e5
Hsiwozer <1377311206@qq.com>
2021-11-30 19:13:49 +0800

    first commit

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(function() {
+
+    $('.icon').on('mouseenter mouseleave', function() {
+        $('.e').slideToggle();
+    })
+    $('.icon').on('click', function() {
+        alert("此时，一道流星划过天空🌠")
+        prompt("许个愿望吧🥳");
+        alert("⭐️心想事成⭐️");
+        alert("愿你三冬暖，愿你春不寒🔥\n愿你天黑有灯🎆，下雨有伞☂️\n愿你一路上，有良人相伴✨\n愿你道路漫长，归来仍是少年💪🏻");
+        alert("愿你抬头看得见月亮🌙，手中握着六便士");
+        alert("生逢盛世🌍\n日新月异🌞🌛\n快心遂意😄\n乐以忘忧🤗");
+        alert("祝你生日快乐🎉🎊🎉🎊🎉🎊🎉🎂");
+    })
+})
